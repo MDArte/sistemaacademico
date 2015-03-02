ad5a1894b4d9afbeadd62079931e209c79ffdecf
Felipe Milepe <fmilepe@gmail.com>
2015-03-02 15:07:06 -0300

    Mudando o consulta para a tabela ajax

--- web/geral/src/java/br/mdarte/exemplo/academico/web/geral/consultarCurso/ConsultaCursoControleImpl.java
+++ web/geral/src/java/br/mdarte/exemplo/academico/web/geral/consultarCurso/ConsultaCursoControleImpl.java
@@ -1,8 +1,12 @@
 
 // license-header java merge-point
 package br.mdarte.exemplo.academico.web.geral.consultarCurso;
+import java.util.Collection;
+
 import br.mdarte.exemplo.academico.util.Constantes;
+
 import org.andromda.presentation.bpm4struts.ViewContainer;
+
 import br.mdarte.exemplo.academico.util.PaginationStrategy;
 
 /**
@@ -11,6 +15,11 @@ import br.mdarte.exemplo.academico.util.PaginationStrategy;
 public class ConsultaCursoControleImpl extends ConsultaCursoControle
 {
     /**
+	 * 
+	 */
+	private static final long serialVersionUID = 5610549811142961010L;
+
+	/**
      * @see br.mdarte.exemplo.academico.web.geral.consultarCurso.ConsultaCursoControle#consultaCurso(br.mdarte.exemplo.academico.web.geral.consultarCurso.ConsultaCursoForm)
      */
     public final void consultaCurso(br.mdarte.exemplo.academico.web.geral.consultarCurso.ConsultaCursoForm form, ViewContainer container) throws Exception {
@@ -20,5 +29,19 @@ public class ConsultaCursoControleImpl extends ConsultaCursoControle
 		*/	
         // nothing to be done for this operation, there are no properties that can be set
     }
+
+	@Override
+	protected Integer getResultadoConsultaCursosTableLength(PaginationStrategy paginacao, String propriedade, Boolean desc, String codigo, String nome, ViewContainer container) throws Exception
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Collection loadResultadoConsultaCursosTable(PaginationStrategy paginacao, String propriedade, Boolean desc, String codigo, String nome, ViewContainer container) throws Exception
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
